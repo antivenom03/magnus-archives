d80557723ef5e6932bf3e5d41af05627570b4864
Jo <jojoivy03@gmail.com>
2023-03-11 02:03:58 +0000

    Deploy

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/magnus-archives";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '316384049852364963162721485403678033239'                       // used to check whether the localStorage data is stale
+var gzip_hash = '118992370205709848227458550307012562671'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
